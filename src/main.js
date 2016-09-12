ed7bc7631b9837913236bdaf0b84aec243272a66
Sam Spilsbury <sam@endlessm.com>
2016-09-12 09:17:45 +0800

    src: Add version checks for Pango and PangoCairo

--- src/main.js
+++ src/main.js
@@ -14,7 +14,9 @@ pkg.require({
     Gtk: '3.0',
     Gio: '2.0',
     GLib: '2.0',
-    GObject: '2.0'
+    GObject: '2.0',
+    Pango: '1.0',
+    PangoCairo: '1.0'
 });
 
 const GLib = imports.gi.GLib;
