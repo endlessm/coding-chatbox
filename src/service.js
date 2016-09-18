ce95f04035220cada639074e40a4f74b34cf49f4
Sam Spilsbury <sam@endlessm.com>
2016-09-19 06:12:29 +0800

    service: Make it clear that responses have text and names

--- src/service.js
+++ src/service.js
@@ -55,12 +55,16 @@ const CodingGameService = new Lang.Class({
         }));
     },
 
-    respond_to_message: function(location, contents, response) {
-        this._service.call_chat_response(location, contents, response, null, Lang.bind(this, function(source, result) {
+    respond_to_message: function(location, response_contents, response_name) {
+        this._service.call_chat_response(location,
+                                         response_contents,
+                                         response_name,
+                                         null,
+                                         Lang.bind(this, function(source, result) {
             try {
                 [success, returnValue] = this._service.call_chat_response_finish(result);
             } catch(e) {
-                logError(e, "Failed to repond to message " + location + " with response " + response);
+                logError(e, "Failed to repond to message " + location + " with response " + response_name);
             }
         }));
     }
