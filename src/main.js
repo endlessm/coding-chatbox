1ab7461e94ce53de7e4f236fc93bea1e0a408ad8
Lili Cosic <lili@kinvolk.io>
2016-12-21 16:41:44 +0100

    main: add support for launching applications
    This enables us to launch applications by passing a .desktop file path.
    https://phabricator.endlessm.com/T14479

--- src/main.js
+++ src/main.js
@@ -438,6 +438,15 @@ const RenderableInputChatboxMessage = new Lang.Class({
     }
 });
 
+// contentType
+//
+// Helper function that returns content type of a GFile.
+function contentType(file){
+    let fileInfo = file.query_info('standard::*', 0, null, null);
+    let type = fileInfo.get_content_type();
+    return type;
+}
+
 const RenderableAttachmentChatboxMessage = new Lang.Class({
     Name: 'RenderableAttachmentChatboxMessage',
     Extends: State.AttachmentChatboxMessage,
@@ -448,8 +457,16 @@ const RenderableAttachmentChatboxMessage = new Lang.Class({
             visible: true
         });
         view.connect('clicked', Lang.bind(this, function() {
-            let handler = this.path.query_default_handler(null);
-            handler.launch([this.path], null);
+            let glist = [];
+            let appInfo = null;
+
+            if (contentType(this.path) == 'application/x-desktop') {
+                appInfo = Gio.DesktopAppInfo.new_from_filename(this.path.get_parse_name());
+            } else {
+                appInfo = this.path.query_default_handler(null);
+                glist = [this.path];
+            }
+            appInfo.launch(glist, null);
 
             listener({
                 response: {
