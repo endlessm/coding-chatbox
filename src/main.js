fc48224d34594fa36963cb475d4708135d6e1c3e
Simon Schampijer <simon.schampijer@endlessm.com>
2016-12-30 22:27:59 +0100

    Set title to ChatBox
    Like we already do in the desktop file for the display name.

--- src/main.js
+++ src/main.js
@@ -542,7 +542,7 @@ const CodingChatboxMainWindow = new Lang.Class({
     _init: function(params) {
         // Force the title of the window to be Coding Chatbox here so that it
         // doesn't get overridden as 'Current Page' later.
-        params.title = _('Coding Chatbox');
+        params.title = _('ChatBox');
         this.parent(params);
 
         this._state = new State.CodingChatboxState(MessageClasses);
